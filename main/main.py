d69d0fb29312504edc22cc0efed574d3e2a6f849
Abir Hasan <abir.spsc@gmail.com>
2021-07-10 19:17:03 +0600

    Add empty REPL. The beginning of the project.

--- main/main.py
+++ main/main.py
@@ -1,6 +1,39 @@
-def main() -> None:
-    """[summary]"""
-    pass
+from pathlib import Path
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.styles import Style
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.lexers import PygmentsLexer
+
+
+word_completer = WordCompleter([], ignore_case=True)
+
+style = Style.from_dict(
+    {
+        "completion-menu.completion": "bg:#008888 #ffffff",
+        "completion-menu.completion.current": "bg:#00aaaa #000000",
+        "scrollbar.background": "bg:#88aaaa",
+        "scrollbar.button": "bg:#222222",
+    }
+)
+
+
+def main():
+    session = PromptSession(completer=word_completer, style=style)
+
+    current_path = str(Path.cwd())
+
+    while True:
+        try:
+            text = session.prompt(current_path + ">")
+        except KeyboardInterrupt:
+            continue
+        except EOFError:
+            break
+        else:
+            print(text)
+
+    print("Exiting...")
 
 
 if __name__ == "__main__":
