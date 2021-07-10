49146c19efd5dd9aab7f044425f51c57b299336e
DavinderJolly <davinderrocks0786@gmail.com>
2021-07-10 20:09:38 +0530

    added parse_commnds function with commands for exit, echo and clear

--- main/main.py
+++ main/main.py
@@ -1,10 +1,14 @@
+import sys
 from pathlib import Path
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.shortcuts import clear
 from prompt_toolkit.styles import Style
 
-word_completer = WordCompleter([], ignore_case=True)
+word_completer = WordCompleter(
+    ["cls", "clear", "exit", "quit", "echo"], ignore_case=True
+)
 
 style = Style.from_dict(
     {
@@ -16,23 +20,44 @@ style = Style.from_dict(
 )
 
 
+def parse_commands(command: str) -> None:
+    """
+    Simple function to parse some commands
+
+    Args:
+        command: a command input by the user
+        session
+    """
+    if command.lower().startswith("echo"):
+        print(command.replace(command.split()[0] + " ", ""))
+
+    elif command.lower() in ["cls", "clear"]:
+        clear()
+
+    elif command.lower() in ["exit", "quit"]:
+        sys.exit()
+
+    else:
+        print(f"command {command.split()[0]} not found")
+
+
 def main() -> None:
     """Starts a REPL in the terminal."""
-    session = PromptSession(completer=word_completer, style=style)
-
-    current_path = str(Path.cwd())
+    session: PromptSession = PromptSession(completer=word_completer, style=style)
 
     while True:
         try:
-            text = session.prompt(current_path + ">")
+            current_path = str(Path.cwd())
+            command = session.prompt(current_path + "> ").strip()
+
+            parse_commands(command)
+
         except KeyboardInterrupt:
             continue
         except EOFError:
             break
-        else:
-            print(text)
 
-    print("Exiting...")
+    sys.exit()
 
 
 if __name__ == "__main__":
