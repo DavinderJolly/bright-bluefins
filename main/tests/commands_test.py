625819227c00cade577ec13742a09c400b059d12
DavinderJolly <davinderrocks0786@gmail.com>
2021-07-14 20:28:12 +0530

    grouping tests in a single class

--- main/tests/commands_test.py
+++ main/tests/commands_test.py
@@ -6,140 +6,141 @@ import pytest
 from .. import commands
 
 
-@pytest.fixture()
-def cmds() -> commands.Commands:
-    """
-    Fixture that creates a Repl object
+class TestCommands:
+    """Tests for Repl commands"""
 
-    Returns: Repl
-    """
-    return commands.Commands(Path.cwd())
+    @pytest.fixture()
+    def cmds(self) -> commands.Commands:
+        """
+        Fixture that creates a Repl object
 
+        Returns: Repl
+        """
+        return commands.Commands(Path.cwd())
 
-def test_cd(cmds: commands.Commands) -> None:
-    """
-    Unit test for the cd command
+    def test_cd(self, cmds: commands.Commands) -> None:
+        """
+        Unit test for the cd command
 
-    Args:
-        cmds: object returned by the Commands class
-    """
-    check_path = cmds.current_path.joinpath("main")
-    cmds.change_dir("./main")
-    assert cmds.current_path == check_path
-    check_path = cmds.current_path.parent
-    cmds.change_dir("..")
-    assert cmds.current_path == check_path
+        Args:
+            cmds: object returned by the Commands class
+        """
+        check_path = cmds.current_path.joinpath("main")
+        cmds.change_dir("./main")
+        assert cmds.current_path == check_path
+        check_path = cmds.current_path.parent
+        cmds.change_dir("..")
+        assert cmds.current_path == check_path
 
+    def test_dir(self, cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
+        """
+        Unit test for the dir command
 
-def test_dir(cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
-    """
-    Unit test for the dir command
+        Args:
+            cmds: object returned by the Commands class
+            capsys: CaptureFixture object
+        """
+        cmds.list_dir()
+        out, err = capsys.readouterr()
+        assert (
+            out == "\n".join([dir.name for dir in cmds.current_path.iterdir()]) + "\n"
+        )
+        assert err == ""
+        cmds.list_dir("./main")
+        out, err = capsys.readouterr()
+        assert (
+            out
+            == "\n".join(
+                [dir.name for dir in cmds.current_path.joinpath("main").iterdir()]
+            )
+            + "\n"
+        )
+        assert err == ""
 
-    Args:
-        cmds: object returned by the Commands class
-        capsys: CaptureFixture object
-    """
-    cmds.list_dir()
-    out, err = capsys.readouterr()
-    assert out == "\n".join([dir.name for dir in cmds.current_path.iterdir()]) + "\n"
-    assert err == ""
-    cmds.list_dir("./main")
-    out, err = capsys.readouterr()
-    assert (
-        out
-        == "\n".join([dir.name for dir in cmds.current_path.joinpath("main").iterdir()])
-        + "\n"
-    )
-    assert err == ""
+    def test_del(self, cmds: commands.Commands) -> None:
+        """
+        Unit test for the del command
 
+        Args:
+            cmds: object returned by the Commands class
+            capsys: CaptureFixture object
+        """
+        path = Path("hello.txt")
+        with path.open("w") as f:
+            f.write("hello world")
 
-def test_del(cmds: commands.Commands) -> None:
-    """
-    Unit test for the del command
+        assert path.exists()
+        cmds.delete_file([str(path)])
+        assert not path.exists()
 
-    Args:
-        cmds: object returned by the Commands class
-        capsys: CaptureFixture object
-    """
-    path = Path("hello.txt")
-    with path.open("w") as f:
-        f.write("hello world")
+    def test_rd(self, cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
+        """
+        Unit test for the rd command
 
-    assert path.exists()
-    cmds.delete_file([str(path)])
-    assert not path.exists()
+        Args:
+            cmds: object returned by the Commands class
+            capsys: CaptureFixture object
+        """
+        path = Path("hello")
+        path.mkdir()
+        assert path.exists() and path.is_dir()
+        cmds.remove_dir(path.name)
+        assert not (path.exists() or path.is_dir())
+        cmds.remove_dir(path.name)
+        out, err = capsys.readouterr()
+        assert out == f"{str(path.resolve())} is not a directory\n"
+        assert err == ""
 
+    def test_type(self, cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
+        """
+        Unit Test for type command
 
-def test_rd(cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
-    """
-    Unit test for the rd command
+        Args:
+            cmds: object returned by the Commands class
+            capsys: CaptureFixture object
+        """
+        path = Path("hello.txt")
+        with path.open("w") as f:
+            f.write("hello world")
+        cmds.show_file_content("hello.txt")
+        out, err = capsys.readouterr()
+        Path.unlink(path)
+        assert out == "hello world\n"
+        assert err == ""
 
-    Args:
-        cmds: object returned by the Commands class
-        capsys: CaptureFixture object
-    """
-    path = Path("hello")
-    path.mkdir()
-    assert path.exists() and path.is_dir()
-    cmds.remove_dir(path.name)
-    assert not (path.exists() or path.is_dir())
-    cmds.remove_dir(path.name)
-    out, err = capsys.readouterr()
-    assert out == f"{str(path.resolve())} is not a directory\n"
-    assert err == ""
+    def test_ren(self, cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
+        """
+        Unit Test for ren command
 
+        Args:
+            cmds: object returned by the Commands class
+            capsys: CaptureFixture object
+        """
+        path = Path("test_ren.txt")
+        with path.open("w") as f:
+            f.write("hello world")
+        new_name = Path("test.txt")
+        cmds.rename(path.name, new_name.name)
+        assert new_name.exists()
+        new_name.unlink()
+        cmds.rename(new_name.name, "fail.txt")
+        out, err = capsys.readouterr()
+        assert out == f"{str(new_name.resolve())} is not a file\n"
+        assert err == ""
 
-def test_type(cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
-    """
-    Unit Test for type command
+    def test_date(self, cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
+        """
+        Unit Test for date command
 
-    Args:
-        cmds: object returned by the Commands class
-        capsys: CaptureFixture object
-    """
-    path = Path("hello.txt")
-    with path.open("w") as f:
-        f.write("hello world")
-    cmds.show_file_content("hello.txt")
-    out, err = capsys.readouterr()
-    Path.unlink(path)
-    assert out == "hello world\n"
-    assert err == ""
-
-
-def test_ren(cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
-    """
-    Unit Test for ren command
-
-    Args:
-        cmds: object returned by the Commands class
-        capsys: CaptureFixture object
-    """
-    path = Path("test_ren.txt")
-    with path.open("w") as f:
-        f.write("hello world")
-    new_name = Path("test.txt")
-    cmds.rename(path.name, new_name.name)
-    assert new_name.exists()
-    new_name.unlink()
-    cmds.rename(new_name.name, "fail.txt")
-    out, err = capsys.readouterr()
-    assert out == f"{str(new_name.resolve())} is not a file\n"
-    assert err == ""
-
-
-def test_date(cmds: commands.Commands, capsys: pytest.CaptureFixture) -> None:
-    """
-    Unit Test for date command
-
-    Args:
-        cmds: object returned by the Commands class
-        capsys: CaptureFixture object"""
-    cmds.get_date()
-    out, err = capsys.readouterr()
-    assert out == datetime.datetime.now().strftime("%d-%m-%Y") + "\n"
-    cmds.get_date("%H:%M")
-    assert err == ""
-    out, err = capsys.readouterr()
-    assert out == datetime.datetime.now().strftime("%H:%M") + "\n"
-    assert err == ""
+        Args:
+            cmds: object returned by the Commands class
+            capsys: CaptureFixture object
+        """
+        cmds.get_date()
+        out, err = capsys.readouterr()
+        assert out == datetime.datetime.now().strftime("%d-%m-%Y") + "\n"
+        cmds.get_date("%H:%M")
+        assert err == ""
+        out, err = capsys.readouterr()
+        assert out == datetime.datetime.now().strftime("%H:%M") + "\n"
+        assert err == ""
