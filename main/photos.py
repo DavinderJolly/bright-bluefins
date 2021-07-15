c0a023ef2c0028b05ea5bfeab3d36d99b4733cd5
Abir Hasan <abir.spsc@gmail.com>
2021-07-16 01:34:26 +0600

    Update image quality in image viewer and add ASCII mode

--- main/photos.py
+++ main/photos.py
@@ -7,6 +7,7 @@ from prompt_toolkit.key_binding.key_processor import KeyPressEvent
 from prompt_toolkit.layout.containers import VSplit, Window
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.output.color_depth import ColorDepth
 
 
 class ImageViewer:
@@ -14,11 +15,9 @@ class ImageViewer:
 
     def __init__(self, path: str, mode: str = "ANSI", size: tuple = (100, 50)):
         self.path = path
-        if mode in ["ANSI", "ASCII"]:
-            self.mode = mode
-        else:
-            print("Supported image mode: ANSI, ASCII")
+        self.mode = mode.upper()
         self.size = size
+        self.image_string: str = ""
 
     @staticmethod
     def rgb2ascii(rgb: list) -> str:
@@ -47,12 +46,8 @@ class ImageViewer:
         """
         return "\x1b[48;2;{};{};{}m".format(r, g, b)
 
-    def view(self) -> None:
-        """View the image"""
-        image = Image.open(self.path)
-        image.thumbnail(self.size, Image.HAMMING)
-
-        string: str = ""
+    def view_ansi(self, image: Image) -> None:
+        """View the image in ANSI mode"""
         for y in range(0, image.height, 2):
             for x in range(0, image.width):
                 try:
@@ -63,18 +58,36 @@ class ImageViewer:
 
                 fg = self.ansi_fg(*fg_color)
                 bg = self.ansi_bg(*bg_color)
-                string += "{}{}▄".format(fg, bg)
-            string += "\n"
+                self.image_string += "{}{}▄".format(fg, bg)
+            self.image_string += "\n"
 
-        app = self.make_app(string)
+    def view_ascii(self, image: Image) -> None:
+        """View the image in ASCII mode"""
+        for y in range(0, image.height):
+            for x in range(0, image.width):
+                try:
+                    color = image.getpixel((x, y))[:3]
+                except IndexError:
+                    continue
+
+                self.image_string += self.rgb2ascii(color)
+            self.image_string += "\n"
+
+    def run_app(self) -> None:
+        """Run the ImageViewer app"""
+        image = Image.open(self.path)
+        image.thumbnail(self.size, Image.HAMMING)
+
+        if self.mode == "ANSI":
+            self.view_ansi(image)
+        elif self.mode == "ASCII":
+            self.view_ascii(image)
+
+        app = self.make_app()
         app.run()
 
-    def make_app(self, string: str) -> Application:
-        """Create the app to show image
-
-        Args:
-            string: Image string in ANSI or ASCII
-        """
+    def make_app(self) -> Application:
+        """Create the app to show image"""
         kb = KeyBindings()
 
         @kb.add("c-d")
@@ -87,15 +100,28 @@ class ImageViewer:
             event.app.exit()
 
         container = VSplit(
-            [Window(content=FormattedTextControl(text=ANSI(string), key_bindings=kb))]
+            [
+                Window(
+                    content=FormattedTextControl(
+                        text=ANSI(self.image_string), key_bindings=kb
+                    )
+                )
+            ]
         )
         layout = Layout(container)
-        app = Application(layout=layout, full_screen=True)
+        app = Application(
+            color_depth=ColorDepth.TRUE_COLOR, layout=layout, full_screen=True
+        )
         return app
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        ImageViewer(sys.argv[1]).view()
+        path = sys.argv[1]
+        if len(sys.argv) > 2:
+            mode = sys.argv[2]
+        else:
+            mode = "ANSI"
+        ImageViewer(path=path, mode=mode).run_app()
     else:
         print("Usage: photos img_path")
